c2c0a0725af0599ab9558fb8ffcd778ac07ff194
raging-loon <cxmacolley@gmail.com>
2022-03-10 13:52:17 -0800

    modified

--- dbtools.ps1
+++ dbtools.ps1
@@ -1,7 +1,15 @@
-# Set-Variable -Name EXCLUSIVE_SORT -Value 1 -Option Constant -Scope Global -Force
-# Set-Variable -Name INCLUSIVE_SORT -Value 2 -Option Constant -Scope Global -Force
+
+# make these constants
 $EXCLUSIVE_SORT = 1
 $INCLUSIVE_SORT = 2
+
+<#
+  @function in_ex_clusive_sort
+  IN/EXclusive sort function
+  Takes the type of sort(users the constants) and sorts based on that
+  Takes a control file($controlfile), used to compare with the $dbfiles and
+  will output it in $output
+#>
 function in_ex_clusive_sort{
   param (
     [int32]$type,
@@ -36,8 +44,23 @@ function in_ex_clusive_sort{
   }
 }
 
+function init_gui(){
+  Add-Type -AssemblyName System.Windows.Forms
+  $dbtools_window = New-Object System.Windows.Forms.Form;
+  $dbtools_window.Text = "DB-Tools";
+  $dbtools_window.AutoSize = $true;
+  $win_toolbar = New-Object System.Windows.Forms.MenuStrip;
+  $openfiles = New-Object System.Windows.Forms.ToolStripMenuItem;
+  $win_toolbar.Items.AddRange(@(
+    $openfiles;
+  ));
+  $win_toolbar.Location = New-Object System.Drawing.Point(0,0);
+  $win_toolbar.Name ="Toolbar";
+  $win_toolbar.TabIndex = 0;
+  $dbtools_window.Controls.Add($win_toolbar)
+  $dbtools_window.ShowDialog();
+  # addhelp
 
+}
 
-$dbarray = @('./db1.txt','./db2.txt')
-# for testing vv
-in_ex_clusive_sort -type $EXCLUSIVE_SORT ./control.txt -dbfiles $dbarray -output ./output.txt
+init_gui
