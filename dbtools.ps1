e7b532f4c85a2d7e6e9fadeea5fc4dc1733f19ad
raging-loon <cxmacolley@gmail.com>
2022-03-10 14:21:56 -0800

    modified

--- dbtools.ps1
+++ dbtools.ps1
@@ -1,6 +1,8 @@
 [void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [void][Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 
+$__controlfile = ""
+$__dbfile = @();
 
 # make these constants
 $EXCLUSIVE_SORT = 1
@@ -20,6 +22,11 @@ function in_ex_clusive_sort{
     [String[]]$dbfiles,
     [String]$output
   )
+  Write-Host $controlfile
+  if($dbfiles.Count -eq 0 -or $dbfiles.Count -gt 4){
+    Write-Host "Too many or not enough database files";
+    return
+  }
   $unique_entries = @();
   for(($i = 0); $i -lt $dbfiles.Count; $i++){
     $dbcontents = Get-Content $dbfiles[$i]
@@ -46,19 +53,18 @@ function in_ex_clusive_sort{
     $entry | Out-File -FilePath $output -Append
   }
 }
-function open_file_menu{
+function open_file_menu($Sender, $fname){
   $browser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
      InitialDirectory = [Environment]::GetFolderPath('Desktop')
     }
   $browser.ShowDialog()
-
-
+  $fname = $browser.FileName
 }
+
 function init_gui(){
   Add-Type -AssemblyName System.Windows.Forms
   # set up variables
-  $__controlfile = ""
-  $__dbfile = @();
+  
 
   # set up main window
   $dbtools_window = New-Object System.Windows.Forms.Form;
@@ -68,32 +74,38 @@ function init_gui(){
   $win_toolbar = New-Object System.Windows.Forms.MenuStrip;
 
   $openfiles = New-Object System.Windows.Forms.ToolStripMenuItem;
-  $open_controlfile = New-Object System.Windows.Forms.ToolStripMenuItem
-  $open_db_file = New-Object System.Windows.Forms.ToolStripMenuItem
+  $open_controlfile = New-Object System.Windows.Forms.ToolStripMenuItem;
+  $open_db_file = New-Object System.Windows.Forms.ToolStripMenuItem;
   $help_item = New-Object System.Windows.Forms.ToolStripMenuItem;
+  $run_tool = New-Object System.Windows.Forms.ToolStripMenuItem;
   # set text
   $openfiles.Text = "File";
   $open_db_file.Text = "Open Database File"
   $open_controlfile.Text = "Open Control File"
   $help_item.Text = "Help"
-  $open_controlfile.Add_Click({open_file_menu})
+  $run_tool.Text = "Run";
+
+  $open_controlfile.Add_Click({open_file_menu $open_controlfile $controlfile})
+  $run_tool.Add_Click({in_ex_clusive_sort -type 1 -controlfile $controlfile -dbfiles $__dbfile -output "output.txt"})
   # add sub menues
   $openfiles.DropDownItems.AddRange(@(
     $open_controlfile,
     $open_db_file
+    
   ))
   # add tool bar items to tool bar
   $win_toolbar.Items.AddRange(@(
     $openfiles,
     $help_item
-  ));
+    $run_tool
+    ));
   $win_toolbar.Location = New-Object System.Drawing.Point(0,0);
   $win_toolbar.Name ="Toolbar";
   $win_toolbar.TabIndex = 0;
   $dbtools_window.Controls.Add($win_toolbar)
   $dbtools_window.ShowDialog();
   # addhelp
-
+  Write-Host $__controlfile
 }
 
 init_gui
