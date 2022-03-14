4790475d058f3e52a9724ae4ae2c46746cf8cfde
raging-loon <cxmacolley@gmail.com>
2022-03-14 08:24:00 -0700

    modified

--- excel-tools.ps1
+++ excel-tools.ps1
@@ -11,7 +11,14 @@ function excel_exclusive_sort{
 	$target_entries = Get-Content $controlfile
 	for(($i = 0); $i -lt $dbfiles.Count; $i++){
 		foreach($line in $target_entries){
-			if((Import-Excel -Path $dbfiles[$i] | WHere-Object $column -eq $line) -eq $null)	
+			if((Import-Excel -Path $dbfiles[$i] | WHere-Object $column -eq $line) -eq $null) {
+				$uniq_ent += $line;
+			}	
 		}
 	}
+	foreach($entry in $uniq_ent){
+		Write-Host $entry
+	}
 }
+$dbfiles = @('.\User Database Test.xlsx')
+excel_exclusive_sort -controlfile '.\control.txt' -dbfiles db_file -column 'Username' -output 'output.txt'  
