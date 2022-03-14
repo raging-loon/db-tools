6a80e0e8521c47ef806dfe6c0ec2a55cfa93a6c9
raging-loon <cxmacolley@gmail.com>
2022-03-14 09:36:09 -0700

    modified

--- excel-tools.ps1
+++ excel-tools.ps1
@@ -1,12 +1,13 @@
 Param(
 	[Parameter()]
+	[Switch]$help,
 	[Switch]$start_gui,
 	[String]$controlfile,
 	[String]$dbfile1,
 	[String]$dbfile2,
 	[String]$dbfile3,
 	[String]$column,
-	[String]$outputfole
+	[String]$output
 )
 
 function excel_exclusive_sort{
@@ -19,6 +20,9 @@ function excel_exclusive_sort{
 	$uniq_ent = @()
 	$target_entries = Get-Content $controlfile
 	for(($i = 0); $i -lt $dbfiles.Count; $i++){
+		if($dbfiles[$i] -eq ''){
+			continue
+		}
 		foreach($line in $target_entries){
 			
 			if(($null -eq (Import-Excel -Path $dbfiles[$i] | WHere-Object $column -eq $line)) -and $uniq_ent -notcontains $line) {
@@ -30,6 +34,21 @@ function excel_exclusive_sort{
 		$entry | Out-File -FilePath $output -Append
 	}
 }
-$dbfiles = @('.\User Database Test.xlsx','.\User Database 2.xlsx')
 
-excel_exclusive_sort -controlfile '.\control.txt' -dbfiles $dbfiles -column 'Username' -output 'output.txt'  
+
+if($help){
+	Write-Host 'List of options for Excel Tools'
+	Write-Host '-help - print this list'
+	Write-Host '-controlfile: - file with entries for comparison'
+	Write-Host '-dbfile1-3: - use dbfile1, dbfile2, etc. These are the database files to run -controlfile against'
+	Write-Host '-column: - column that should be checked'
+	Write-Host '-output: - Saves the results in a file of your choice'
+	Write-Host '-start_gui - Start the GUI, still in developmnet'
+} elseif($start_gui){
+	Write-Host "GUI Still in development"
+	exit
+} else {
+	$dbfiles = @($dbfile1, $dbfile2,$dbfile3)
+	excel_exclusive_sort -controlfile $controlfile -dbfiles $dbfiles -column $column -output $output
+
+}
