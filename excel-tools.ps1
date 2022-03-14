42800a6b1d5cce3749bb4f46e856f3a3e859ccbc
raging-loon <cxmacolley@gmail.com>
2022-03-14 09:02:30 -0700

    modified

--- excel-tools.ps1
+++ excel-tools.ps1
@@ -1,4 +1,13 @@
-
+Param(
+	[Parameter()]
+	[Switch]$start_gui,
+	[String]$controlfile,
+	[String]$dbfile1,
+	[String]$dbfile2,
+	[String]$dbfile3,
+	[String]$column,
+	[String]$outputfole
+)
 
 function excel_exclusive_sort{
 	Param(
@@ -11,14 +20,16 @@ function excel_exclusive_sort{
 	$target_entries = Get-Content $controlfile
 	for(($i = 0); $i -lt $dbfiles.Count; $i++){
 		foreach($line in $target_entries){
-			if((Import-Excel -Path $dbfiles[$i] | WHere-Object $column -eq $line) -eq $null) {
+			
+			if(($null -eq (Import-Excel -Path $dbfiles[$i] | WHere-Object $column -eq $line)) -and $uniq_ent -notcontains $line) {
 				$uniq_ent += $line;
 			}	
 		}
 	}
 	foreach($entry in $uniq_ent){
-		Write-Host $entry
+		$entry | Out-File -FilePath $output -Append
 	}
 }
-$dbfiles = @('.\User Database Test.xlsx')
-excel_exclusive_sort -controlfile '.\control.txt' -dbfiles db_file -column 'Username' -output 'output.txt'  
+$dbfiles = @('.\User Database Test.xlsx','.\User Database 2.xlsx')
+
+excel_exclusive_sort -controlfile '.\control.txt' -dbfiles $dbfiles -column 'Username' -output 'output.txt'  
